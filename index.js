8344e58e63604746394f0aaadde423ca81d7ee13
J1MMM <jimuelbaraero00@gmail.com>
2022-11-19 18:41:49 +0800

    responsive

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const menuIcon = document.getElementById("menu-icon");
+const nav = document.getElementById("nav");
 
-menuIcon.addEventListener('click', ()=>{
-    console.log("clicked");
+
+nav.addEventListener('click', () => {
     menuIcon.classList.toggle("toggle")
+    nav.classList.toggle("open");
 })
